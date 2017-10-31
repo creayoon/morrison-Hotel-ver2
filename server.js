cd5c9ce95351c44e235b74d72b0b3482cdd8b3a2
creayoon <Jyoon@olmangnim-3.local>
2017-11-01 01:43:54 +0900

    typescript 도입 에러 상태

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 require('source-map-support').install();
 require('./dist-server/server/index.js').default();
+require("babel-core").transform("code", {
+  presets: ["typescript"]
+});
