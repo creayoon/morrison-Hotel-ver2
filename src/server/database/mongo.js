9118ec005338eb3a56339cb0ac2a44cd5cf0413d
creayoon <Jyoon@olmangnim-3.local>
2017-11-08 04:48:53 +0900

    Revert "non-zero status로 test는 fail 상태"
    This reverts commit a25e3d34420189529ea18af2909194f2b9e24190.

--- src/server/database/mongo.js
+++ src/server/database/mongo.js
@@ -2,7 +2,6 @@ import mongodb from 'mongodb';
 import mongoMock from 'mongo-mock';
 import config from 'config';
 import fs from 'fs';
-import { Logger } from 'winston';
 
 export const MongoClient =
     process.env.NODE_ENV === 'test' ? mongoMock.MongoClient : mongodb.MongoClient;
@@ -11,118 +10,74 @@ const mongoConfig = JSON.parse(fs.readFileSync(config.mongodb, 'utf-8'));
 // 서버가 돌기 전이라서 sync걸어도 좋아서 sync씀
 
 export class MongoDB {
-  static insert(collection, ...values) {
+  static insert(cb, collection, ...values) {
     if (!collection || !values) {
-      return Promise.reject(new Error('Invalid argument exception'));
+      cb(new Error('error'));
+      return;
     }
 
-    return new Promise((resolve, reject) => {
-      MongoClient.connect(mongoConfig.url, (connErr, db) => {
-        if (connErr) {
-          // db.close(); // 여기서 db.close 안해줘도 됨? yc브랜치에 없음
-          reject(connErr);
-          return;
-        }
-        resolve(db);
-      });
-    })
-    .then(db => {
+    MongoClient.connect(mongoConfig.url, (connErr, db) => {
+      if (connErr) {
+        cb(connErr);
+        return;
+      }
+
       db.collection(collection).insertMany(values, (dbErr, res) => {
         if (dbErr) {
-          db.close(); // 여기서 db.close 안해줘도 됨? yc브랜치에 없음
-          throw dbErr;
-          // new Error(dbErr);
-          // 그냥 throw dbErr 했을떄랑 차이점?
-          // 위에서 error가 이미 생성되서 new 할 필요 없어서 그냥 throw?
-          // return;
+          cb(dbErr);
+          return;
         }
 
         // data가 깨지거나 했을 경우를 대비
         if (values.length !== res.insertedCount) {
-          throw new Error('fail insert');
-          // return;
+          cb(new Error('fail insert'));
+          // 원래는 rollback코드도 추가하는것이 좋다
+          return;
         }
 
         db.close();
-        // cb();
+
+        cb();
       });
-    })
-    .catch(connErr => {
-      Logger.error('insert Error:::', connErr);
     });
   }
 
-  static delete(collection, ...values) {
+  static delete(cb, collection, ...values) {
     if (!collection || !values) {
-      return new Promise.reject(new Error('error'));
+      cb(new Error('error'));
+      return;
     }
 
-    return new Promise ((resolve, reject) => {
-      MongoClient.connect(mongoConfig.url, (connErr, db) => {
-        if (connErr) {
-          // db.close(); // 여기서 db.close 안해줘도 됨?
-          reject(connErr);
-          return;
-        }
-        resolve(db);
-      });
-    })
-    .then(db => {
+    MongoClient.connect(mongoConfig.url, (connErr, db) => {
+      if (connErr) {
+        db.close();
+        cb(connErr);
+        return;
+      }
+
+      // delete, remove 차이점 찾기
+      // query 쓰는 방법 익히기
+      // db.collection(collection).deleteMany(values, (dbErr, res) => {
       db.collection(collection).deleteMany(values[0], (dbErr, res) => {
         if (dbErr) {
           db.close();
-          // cb(dbErr);
-          throw dbErr;
-          // return; //
+          cb(dbErr);
+          return;
         }
 
         // delete error 대비
         if (values.length !== res.deletedCount) {
           db.close();
-          throw new Error('fail delete');
-
-          // 추후 delete rollback, mongo에서 기본 제공하지 않음, 일단 pass
-          // return;
+          cb(new Error('fail delete'));
+          // 추후 delete rollback
+          // mongo에서 기본 제공하지 않음 -> 삭제하기 전 복사해뒀다가 다시 되살리는 방식으로 rollback해야 함
+          return;
         }
         db.close();
-        // cb();
+
+        cb();
       });
-    })
-    .catch(connErr => {
-      Logger.error('delete Error:::', connErr);
     });
   }
 }
 
-// promise 수정 전 코드
-  // static insert(cb, collection, ...values) {
-  //   if (!collection || !values) {
-  //     cb(new Error('error'));
-  //     return;
-  //   }
-
-  //   MongoClient.connect(mongoConfig.url, (connErr, db) => {
-  //     if (connErr) {
-  //       cb(connErr);
-  //       return;
-  //     }
-
-  //     db.collection(collection).insertMany(values, (dbErr, res) => {
-  //       if (dbErr) {
-  //         cb(dbErr);
-  //         return;
-  //       }
-
-  //       // data가 깨지거나 했을 경우를 대비
-  //       if (values.length !== res.insertedCount) {
-  //         cb(new Error('fail insert'));
-  //         // 원래는 rollback코드도 추가하는것이 좋다
-  //         return;
-  //       }
-
-  //       db.close();
-
-  //       cb();
-  //     });
-  //   });
-  // }
