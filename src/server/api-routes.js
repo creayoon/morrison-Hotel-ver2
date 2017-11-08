d407a22fa066cd24c08d45cd7e906f7467723294
creayoon <Jyoon@olmangnim-3.local>
2017-11-09 04:06:18 +0900

    왜 404 error 뜨지..?

--- src/server/api-routes.js
+++ src/server/api-routes.js
@@ -7,4 +7,11 @@ router.get('/', Controller.get);
 router.get('/users', UserController.get);
 router.get('/users/:id', UserController.getById);
 
+router.post('/users', UserController.post);
+router.put('/users', UserController.put);
+router.delete('/users', UserController.delete);
+
+router.delete('/clear', UserController.clear);
+
+
 export default router;
--- src/server/controllers/user-controller.js
+++ src/server/controllers/user-controller.js
@@ -10,9 +10,37 @@ export default class UserController {
   static getById(req, res, cb) {
     const {id} = req.params;
 
-    // res.send({data: data});
     res.send({id});
     cb();
+  }
+
+  static post(req, res, cb) {
+    const {data} = req.query;
+
+    res.send({data});
+    cb(); 
+  }
+
+  static put(req, res, cb) {
+    const {data} = req.query;
+
+    res.send({data});
+    cb(); 
+  }
+
+  static delete(req, res, cb) {
+    const {data} = req.query;
+
+    res.send({data});
+    cb(); 
+  }
+
+  // 어떻게 해야 하나..
+  static clear(req, res, cb) {
+    // const {data} = req.query;
+
+    // res.send({data});
+    cb(); 
   }
 
 }
