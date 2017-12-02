1f4da0721644b6420d8b517d0d24dfbad7934120
Youngchan Kim <ovekyc@gmail.com>
2017-12-02 12:37:54 +0900

    Edit indent size

--- src/server/controllers/room-controller.js
+++ src/server/controllers/room-controller.js
@@ -1,37 +1,36 @@
 export default class RoomController {
-	static get(req, res, cb) {
-		const { data } = req.query;
+  static get(req, res, cb) {
+	const { data } = req.query;
 
-		res.send({ data });
-		cb();
-	}
+	res.send({ data });
+	cb();
+  }
 
-	static post(req, res, cb) {
-		const { data } = req.query;
+  static post(req, res, cb) {
+	const { data } = req.query;
 
-		res.send({ data });
-		cb();
-	}
+	res.send({ data });
+	cb();
+  }
 
-	static getById(req, res, cb) {
-		const { id } = req.params;
+  static getById(req, res, cb) {
+	const { id } = req.params;
 
-		res.send({ id });
-		cb();
-	}
+	res.send({ id });
+	cb();
+  }
 
-	static put(req, res, cb) {
-		const { data } = req.query;
+  static put(req, res, cb) {
+	const { data } = req.query;
 
-		res.send({ data });
-		cb();
-	}
+	res.send({ data });
+	cb();
+  }
 
-	static delete(req, res, cb) {
-		const { data } = req.query;
+  static delete(req, res, cb) {
+	const { data } = req.query;
 
-		res.send({ data });
-		cb();
-	}
-
+	res.send({ data });
+	cb();
+  }
 }
--- src/server/controllers/user-controller.js
+++ src/server/controllers/user-controller.js
@@ -49,97 +49,120 @@ export default class UserController {
 			return UserService.getUser(name);
 		})
 		.then(addUserResult => {
-			res.send(200, addUserResult);
-			cb(); // resolve(res) 아니고 cb()
+          res.send(200, addUserResult);
+          cb(); // resolve(res) 아니고 cb()
 		})
 		.catch(err => {
-			if (err instanceof UserError) {
-				res.send(400, 'Need essential argument');
-				cb();
-				return;
-			}
-			cb(err);
+          if (err instanceof UserError) {
+            res.send(400, 'Need essential argument');
+            cb();
+            return;
+          }
+          cb(err);
 		});
-	}
+  }
 
-	// put: input type check, find user by id, update user info
-	static put(req, res, cb) {
-		const { body } = req;
+  // get by name
+  static getByName(req, res, cb) {
+    const { name } = req.query;
+    // console.log('name:::::', name)
 
-		UserController.validCheck(body)
-			.then(isValid => {
-				if (!isValid) throw new UserError('Need essential argument');
-				return UserService.updateUser(body, res)
-			})
-			.then(addUserResult => {
-				res.send(200, addUserResult);
-				cb(); // resolve(res) 아니고 cb()
-			})
-			.catch(err => {
-				if (err instanceof UserError) {
-					res.send(400, 'Need essential argument');
-					cb();
-					return;
-				}
-				cb(err);
-			});
-	}
+    UserController.validCheck(body)
+        .then(isValid => {
+          if (!isValid) throw new UserError('Need essential argument');
+          return UserService.getUser(name);
+        })
+        .then(addUserResult => {
+          res.send(200, addUserResult);
+          cb(); // resolve(res) 아니고 cb()
+        })
+        .catch(err => {
+          if (err instanceof UserError) {
+            res.send(400, 'Need essential argument');
+            cb();
+            return;
+          }
+          cb(err);
+        });
+  }
+
+  // put: input type check, find user by id, update user info
+  static put(req, res, cb) {
+    const { body } = req;
+
+    UserController.validCheck(body)
+        .then(isValid => {
+          if (!isValid) throw new UserError('Need essential argument');
+          return UserService.updateUser(body, res)
+        })
+        .then(addUserResult => {
+          res.send(200, addUserResult);
+          cb(); // resolve(res) 아니고 cb()
+        })
+        .catch(err => {
+          if (err instanceof UserError) {
+            res.send(400, 'Need essential argument');
+            cb();
+            return;
+          }
+          cb(err);
+        });
+  }
 
 	// delete
 	static delete(req, res) {
-		const { body } = req;
+      const { body } = req;
 
-		UserController.validCheck(body)
-			.then(isValid => {
-				if (!isValid) throw new UserError('Need essential argument');
-				// return UserService.addUser(body, res)
-				return UserService.deleteUser(body, res)
-			})
-			.then(addUserResult => {
-				res.send(200, addUserResult);
-				cb(); // resolve(res) 아니고 cb()
-			})
-			.catch(err => {
-				if (err instanceof UserError) {
-					res.send(400, 'Need essential argument');
-					cb();
-					return;
-				}
-				cb(err);
-			});
+      UserController.validCheck(body)
+          .then(isValid => {
+            if (!isValid) throw new UserError('Need essential argument');
+            // return UserService.addUser(body, res)
+            return UserService.deleteUser(body, res)
+          })
+          .then(addUserResult => {
+            res.send(200, addUserResult);
+            cb(); // resolve(res) 아니고 cb()
+          })
+          .catch(err => {
+            if (err instanceof UserError) {
+              res.send(400, 'Need essential argument');
+              cb();
+              return;
+            }
+            cb(err);
+          });
 	}
 
 	// user model valid check of every single field
 	static validCheck(body) {
-		// console.log('validCheck body:::', body)
+      // console.log('validCheck body:::', body)
 
-		return new Promise((resolve, reject) => {
-			const essentialFields = ['name', 'social', 'image'];
-			const isValid = essentialFields
-				.map(fieldName => {
-					// console.log('fieldName:::', fieldName)
-					if (!body.hasOwnProperty(fieldName)) return false;
-					if (typeof body[fieldName] !== 'string') return false;
-					return true;
-				})
-				.reduce((a, b) => a & b)
-
-			if (!isValid) {
-				// console.log('is not valid:::::::')
-				reject(new Error('is not valid:::::::'));
-			} else {
-				// console.log('is valid:::::::')
-				resolve(isValid)
-			}
-		})
+      return new Promise((resolve, reject) => {
+        const essentialFields = ['name', 'social', 'image'];
+        const isValid = essentialFields
+            .map(fieldName => {
+              // console.log('fieldName:::', fieldName)
+              if (!body.hasOwnProperty(fieldName)) return false;
+              if (typeof body[fieldName] !== 'string') return false;
+              return true;
+            })
+            .reduce((a, b) => a & b)
+        if (!isValid) {
+          // console.log('is not valid:::::::')
+          reject(new Error('is not valid:::::::'));
+        } else {
+          // console.log('is valid:::::::')
+          resolve(isValid)
+        }
+      })
 	}
 }
 
 
 // error
 class UserError extends Error {
-	constructor(message) {
-		super();
-		this.message = message;
-	}
+  constructor(message) {
+    super();
+    this.message = message;
+  }
 }
