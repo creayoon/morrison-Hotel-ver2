3635d45f40b2cbe0f0c9536a55aa63fe45d4df65
Youngchan Kim <ovekyc@gmail.com>
2017-12-02 12:46:02 +0900

    [test code] before

--- src/server/controllers/user-controller.js
+++ src/server/controllers/user-controller.js
@@ -2,64 +2,40 @@ import UserService from '../services/user-service';
 
 
 export default class UserController {
-	// get
-	static get(req, res, cb) {
-		const { data } = req.query;
+  // get
+  static get(req, res, cb) {
+    const { data } = req.query;
 
-		// if (type === 'list')
-		// 	UserService.getAllUser();
-		// else if (type === 'name')
-		// 	UserService.getUserByName(name);
+    // if (type === 'list')
+    // 	UserService.getAllUser();
+    // else if (type === 'name')
+    // 	UserService.getUserByName(name);
 
-		// res.send({ data });
-		cb();
-	}
+    // res.send({ data });
+    cb();
+  }
 
-	// post
-	static post(req, res, cb) {
-		const { body } = req;
+  // post
+  static post(req, res, cb) {
+    const { body } = req;
 
-		UserController.validCheck(body)
-			.then(isValid => {
-				if (!isValid) throw new UserError('Need essential argument');
-				return UserService.addUser(body, res)
-			})
-			.then(addUserResult => {
-				res.send(200, addUserResult);
-				cb(); // resolve(res) 아니고 cb()
-			})
-			.catch(err => {
-				if (err instanceof UserError) {
-					res.send(400, 'Need essential argument');
-					cb();
-					return;
-				}
-				cb(err);
-			});
-	}
-
-	// get by name
-	static getByName(req, res, cb) {
-		const { name } = req.query;
-		// console.log('name:::::', name)
-
-		UserController.validCheck(body)
-		.then(isValid => {
-			if (!isValid) throw new UserError('Need essential argument');
-			return UserService.getUser(name);
-		})
-		.then(addUserResult => {
+    UserController.validCheck(body)
+        .then(isValid => {
+          if (!isValid) throw new UserError('Need essential argument');
+          return UserService.addUser(body, res)
+        })
+        .then(addUserResult => {
           res.send(200, addUserResult);
           cb(); // resolve(res) 아니고 cb()
-		})
-		.catch(err => {
+        })
+        .catch(err => {
           if (err instanceof UserError) {
             res.send(400, 'Need essential argument');
             cb();
             return;
           }
           cb(err);
-		});
+        });
   }
 
   // get by name
@@ -109,53 +85,51 @@ export default class UserController {
         });
   }
 
-	// delete
-	static delete(req, res) {
-      const { body } = req;
+  // delete
+  static delete(req, res) {
+    const { body } = req;
 
-      UserController.validCheck(body)
-          .then(isValid => {
-            if (!isValid) throw new UserError('Need essential argument');
-            // return UserService.addUser(body, res)
-            return UserService.deleteUser(body, res)
+    UserController.validCheck(body)
+        .then(isValid => {
+          if (!isValid) throw new UserError('Need essential argument');
+          // return UserService.addUser(body, res)
+          return UserService.deleteUser(body, res)
+        })
+        .then(addUserResult => {
+          res.send(200, addUserResult);
+          cb(); // resolve(res) 아니고 cb()
+        })
+        .catch(err => {
+          if (err instanceof UserError) {
+            res.send(400, 'Need essential argument');
+            cb();
+            return;
+          }
+          cb(err);
+        });
+  }
+
+  // user model valid check of every single field
+  static validCheck(body) {
+    // console.log('validCheck body:::', body)
+
+    return new Promise((resolve, reject) => {
+      const essentialFields = ['name', 'social', 'image'];
+      const isValid = essentialFields
+          .map(fieldName => {
+            // console.log('fieldName:::', fieldName)
+            if (!body.hasOwnProperty(fieldName)) return false;
+            if (typeof body[fieldName] !== 'string') return false;
+            return true;
           })
-          .then(addUserResult => {
-            res.send(200, addUserResult);
-            cb(); // resolve(res) 아니고 cb()
-          })
-          .catch(err => {
-            if (err instanceof UserError) {
-              res.send(400, 'Need essential argument');
-              cb();
-              return;
-            }
-            cb(err);
-          });
-	}
-
-	// user model valid check of every single field
-	static validCheck(body) {
-      // console.log('validCheck body:::', body)
-
-      return new Promise((resolve, reject) => {
-        const essentialFields = ['name', 'social', 'image'];
-        const isValid = essentialFields
-            .map(fieldName => {
-              // console.log('fieldName:::', fieldName)
-              if (!body.hasOwnProperty(fieldName)) return false;
-              if (typeof body[fieldName] !== 'string') return false;
-              return true;
-            })
-            .reduce((a, b) => a & b)
-        if (!isValid) {
-          // console.log('is not valid:::::::')
-          reject(new Error('is not valid:::::::'));
-        } else {
-          // console.log('is valid:::::::')
-          resolve(isValid)
-        }
-      })
-	}
+          .reduce((a, b) => a & b);
+      if (!isValid) {
+        reject(new Error('is not valid:::::::'));
+      } else {
+        resolve(isValid)
+      }
+    })
+  }
 }
 
 
